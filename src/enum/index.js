dda7b912699f079e08f32a8b8d7eb231b14d1a3d
David Kern <david33belle@gmail.com>
2020-03-18 10:25:40 +0200

    Update index.js

--- src/enum/index.js
+++ src/enum/index.js
@@ -2,3 +2,5 @@
 export const API_URL = 'http://private-05627-frontendnewhire.apiary-mock.com/';
 //default image
 export const unloadedSrc = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTspJjFyqzD-rGXBRVTVtLPT-bdGtt5P9Mwac7rfBOiKEvPSwqG';
+//empty list image
+export const emptyList = 'https://assets-ouch.icons8.com/preview/819/a75c989b-ea17-456d-9e9a-a6a2d07c8428.png';

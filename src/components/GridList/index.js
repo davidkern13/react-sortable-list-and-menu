27b4e265f9e676758396cb3a0517fa2492a001f3
David Kern <david33belle@gmail.com>
2020-03-18 10:33:46 +0200

    Update index.js

--- src/components/GridList/index.js
+++ src/components/GridList/index.js
@@ -14,32 +14,34 @@ const default_email = 32;
 
 const GridList = ({ list = [] }) => {
     return (
-     <div id={'GridList'} className={'grid-container'}>
-        {
-            list.length && list ?
-            list.map( (item, idx) => {
-                return (
-                    <div className={'grid-item'} key={item.name + idx}>
-                        <div className={'item-wrap'}>
-                            <div className={'item-img'}>
-                                <LazyImage unloadedSrc={unloadedSrc} src={item.profile_image} alt={`${item.name}`} />
-                            </div>
-                            <div className={'item-content'}>
-                                <Img src={getLogoByDriverType(item.driverType)} alt={item.name} />
-                                <h3>{charLimit(item.name, default_chars)}</h3>
-                                <p>Driver rank: {charLimit(item.driverRank, default_chars)}</p>
-                                <p>Phone number: {charLimit(item.phone, default_phone)}</p>
-                                <p>Email: {charLimit(item.phone, default_email)}</p>
+        <>
+         <div id={'GridList'} className={'grid-container'}>
+            {
+                list.length > 0 && list && list.map( (item, idx) => {
+                    return (
+                        <div className={'grid-item'} key={item.name + idx}>
+                            <div className={'item-wrap'}>
+                                <div className={'item-img'}>
+                                    <LazyImage unloadedSrc={unloadedSrc} src={item.profile_image} alt={`${item.name}`} />
+                                </div>
+                                <div className={'item-content'}>
+                                    <Img src={getLogoByDriverType(item.driverType)} alt={item.name} />
+                                    <h3>{charLimit(item.name, default_chars)}</h3>
+                                    <p>Driver rank: {charLimit(item.driverRank, default_chars)}</p>
+                                    <p>Phone number: {charLimit(item.phone, default_phone)}</p>
+                                    <p>Email: {charLimit(item.phone, default_email)}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                );
-            })
-            :
-            <div className={'EmptyList'}><Img src={emptyList} alt={'List is empty'}/> <p>List is empty</p></div>
-        }
+                    );
+                })
+            }
+         </div>
 
-     </div>
+        { !list.length && <div className={'EmptyList'}><Img src={emptyList} alt={'List is empty'}/> <p>List is empty</p></div>}
+
+     </>
+
     );
 }
 
